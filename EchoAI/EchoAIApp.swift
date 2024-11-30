70f594dc854bcdc217454790d535c253ef3ad0cc
Elaidzha1940 <eli080293@gmail.com>
2024-11-30 19:27:03 +0300

    [Feature] Update project setting

---
+++ EchoAI/EchoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoAIApp.swift
+//  EchoAI
+//
+//  Created by Elaidzha Shchukin on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct EchoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EchoAITests/EchoAITests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoAITests.swift
+//  EchoAITests
+//
+//  Created by Elaidzha Shchukin on 30.11.2024.
+//
+
+import Testing
+@testable import EchoAI
+
+struct EchoAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
