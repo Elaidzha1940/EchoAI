a11bf8a8df7f76cdbd1802eff89e5ba8945d8bec
Elaidzha1940 <eli080293@gmail.com>
2024-12-09 19:53:57 +0300

    feat: Update CarouselView

---
+++ EchoAI/Components/Views/CategoryCellView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCellView.swift
+//  EchoAI
+//
+//  Created by Elaidzha Shchukin on 09.12.2024.
+//
+
+import SwiftUI
+
+struct CategoryCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryCellView()
+}
