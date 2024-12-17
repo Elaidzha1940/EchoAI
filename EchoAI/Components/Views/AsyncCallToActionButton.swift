c6b0a559e155957022f0a94db0a7507d2e42879a
Elaidzha1940 <eli080293@gmail.com>
2024-12-17 03:13:50 +0300

    feat: CreateAvatarView

---
+++ EchoAI/Components/Views/AsyncCallToActionButton.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncCallToActionButton.swift
+//  EchoAI
+//
+//  Created by Elaidzha Shchukin on 17.12.2024.
+//
+
+import SwiftUI
+
+struct AsyncCallToActionButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncCallToActionButton()
+}
