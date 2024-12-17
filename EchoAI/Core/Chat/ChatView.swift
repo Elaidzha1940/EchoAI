5b9e5ba01a661e8d38f67b9acbdb5074823202d8
Elaidzha1940 <eli080293@gmail.com>
2024-12-17 21:50:00 +0300

    feat: ChatView, ScrollView

--- EchoAI/Core/Chat/ChatView.swift
+++ EchoAI/Core/Chat/ChatView.swift
@@ -15,30 +15,39 @@ struct ChatView: View {
     @State private var currentUser: UserModel? = .mock
     @State private var textField: String = ""
     
+    @State private var showChatSettings: Bool = false
+    @State private var scrollPosition: String?
+    
     var body: some View {
         VStack(spacing: 0) {
             scrollViewSection
-            TextField("Saysomething...", text: $textField)
-                .keyboardType(.alphabet)
-                .autocorrectionDisabled()
-                .padding(12)
-                .background(
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 100)
-                            .fill(Color(uiColor: .systemBackground))
-                        
-                        RoundedRectangle(cornerRadius: 100)
-                            .stroke(Color.gray.opacity(0.03), lineWidth: 1)
-                    }
-                )
-                .padding(.horizontal, 12)
-                .padding(.vertical, 6)
-                .background(Color(uiColor: .secondarySystemBackground))
+            texFieldSection
         }
         .navigationTitle(avatar?.name ?? "Chat")
         .toolbarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Image(systemName: "ellipsis")
+                    .padding(8)
+                    .anyButton {
+                        onChatSettingsPressed()
+                    }
+            }
+        }
+        .confirmationDialog("What would you like to do?", isPresented: $showChatSettings) {
+            Button("Report User / Chat", role: .destructive) {
+                //
+            }
+            
+            Button("Delete Chat", role: .destructive) {
+                //
+            }
+        } message: {
+            Text("What would you like to do?")
+        }
     }
     
+    // MARK: - scrollViewSection
     private var scrollViewSection: some View {
         ScrollView {
             LazyVStack(spacing: 24) {
@@ -48,11 +57,71 @@ struct ChatView: View {
                         message: message,
                         isCurrentUser: isCurrentUser,
                         imageName: isCurrentUser ? nil : avatar?.profileImageName)
+                    .id(message.id)
                 }
             }
             .frame(maxWidth: .infinity)
             .padding(8)
+            .rotationEffect(.degrees(180))
         }
+        .rotationEffect(.degrees(180))
+        .scrollPosition(id: $scrollPosition, anchor: .center)
+        .animation(.default, value: chatMessages.count)
+        .animation(.default, value: scrollPosition)
+    }
+    
+    // MARK: - texFieldSection
+    private var texFieldSection: some View {
+        TextField("Saysomething...", text: $textField)
+            .keyboardType(.alphabet)
+            .autocorrectionDisabled()
+            .padding(12)
+            .padding(.trailing, 60)
+            .overlay(
+                Image(systemName: "arrow.up.circle.fill")
+                    .font(.system(size: 32))
+                    .padding(.trailing, 4)
+                    .foregroundStyle(.accent)
+                    .anyButton(.plain) {
+                        onSendMessagePressed()
+                    }
+                , alignment: .trailing
+            )
+            .background(
+                ZStack {
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(Color(uiColor: .systemBackground))
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .stroke(Color.gray.opacity(0.03), lineWidth: 1)
+                }
+            )
+            .padding(.horizontal, 12)
+            .padding(.vertical, 6)
+            .background(Color(uiColor: .secondarySystemBackground))
+    }
+    
+    // MARK: - onSendMessagePressed
+    private func onSendMessagePressed() {
+        guard let currentUser else { return }
+        let content = textField
+        
+        let message = ChatMessageModel(
+            id: UUID().uuidString,
+            chatId: UUID().uuidString,
+            authorId: currentUser.userId,
+            content: content,
+            seenByIds: nil,
+            dateCreated: .now)
+        chatMessages.append(message)
+        
+        scrollPosition = message.id
+        textField = ""
+    }
+    
+    // MARK: - onChatSettingsPressed
+    private func onChatSettingsPressed() {
+        showChatSettings = true
     }
 }
 
