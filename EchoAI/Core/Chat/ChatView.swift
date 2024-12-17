4976a95523b8e9323c03d58d32f547574249736b
Elaidzha1940 <eli080293@gmail.com>
2024-12-17 20:57:48 +0300

    feat: ChatView

--- EchoAI/Core/Chat/ChatView.swift
+++ EchoAI/Core/Chat/ChatView.swift
@@ -13,28 +13,46 @@ struct ChatView: View {
     @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
     @State private var avatar: AvatarModel? = .mock
     @State private var currentUser: UserModel? = .mock
+    @State private var textField: String = ""
     
     var body: some View {
         VStack(spacing: 0) {
-            ScrollView {
-                LazyVStack(spacing: 24) {
-                    ForEach(chatMessages) { message in
-                        let isCurrentUser = message.authorId == currentUser?.userId
-                        ChatBubbleViewBuilder(
-                            message: message,
-                            isCurrentUser: isCurrentUser,
-                            imageName: isCurrentUser ? nil : avatar?.profileImageName)
+            scrollViewSection
+            TextField("Saysomething...", text: $textField)
+                .keyboardType(.alphabet)
+                .autocorrectionDisabled()
+                .padding(12)
+                .background(
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(Color(uiColor: .systemBackground))
+                        
+                        RoundedRectangle(cornerRadius: 100)
+                            .stroke(Color.gray.opacity(0.03), lineWidth: 1)
                     }
-                }
-                .frame(maxWidth: .infinity)
-                .padding(8)
-            }
-            
-            Rectangle()
-                .frame(height: 50)
+                )
+                .padding(.horizontal, 12)
+                .padding(.vertical, 6)
+                .background(Color(uiColor: .secondarySystemBackground))
         }
         .navigationTitle(avatar?.name ?? "Chat")
         .toolbarTitleDisplayMode(.inline)
+    }
+    
+    private var scrollViewSection: some View {
+        ScrollView {
+            LazyVStack(spacing: 24) {
+                ForEach(chatMessages) { message in
+                    let isCurrentUser = message.authorId == currentUser?.userId
+                    ChatBubbleViewBuilder(
+                        message: message,
+                        isCurrentUser: isCurrentUser,
+                        imageName: isCurrentUser ? nil : avatar?.profileImageName)
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(8)
+        }
     }
 }
 
