7dd78c411cb281f1465d4481dd2df68547d91bce
Elaidzha1940 <eli080293@gmail.com>
2024-12-20 04:28:59 +0300

    feat: Routing -> Navigation Destination -> ExploreView

--- EchoAI/Core/Explore/ExploreView.swift
+++ EchoAI/Core/Explore/ExploreView.swift
@@ -9,19 +9,34 @@
 
 import SwiftUI
 
+enum NavigationPathOption: Hashable {
+    case chat(avatarId: String)
+    case category(category: CharacterOption, imageName: String)
+}
+
 struct ExploreView: View {
     @State private var featuredAvatars: [AvatarModel] = AvatarModel.mocks
     @State private var categories: [CharacterOption] = CharacterOption.allCases
     @State private var popularAvatars: [AvatarModel] = AvatarModel.mocks
     
+    @State private var path: [NavigationPathOption] = []
+    
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 featuredSection
                 categorySection
                 popularSection
             }
             .navigationTitle("Explore")
+            .navigationDestination(for: NavigationPathOption.self) { newValue in
+                switch newValue {
+                case .chat(avatarId: let avatarId):
+                    ChatView(avatarId: avatarId)
+                case .category(category: let category, imageName: let imageName):
+                    CategoryListView(category: category, imageName: imageName)
+                }
+            }
         }
     }
     
@@ -35,7 +50,7 @@ struct ExploreView: View {
                         subTitle: avatar.characterDescription,
                         imageName: avatar.profileImageName)
                     .anyButton {
-                        
+                        onAvatarPressed(avatar: avatar)
                     }
                 }
             }
@@ -50,13 +65,16 @@ struct ExploreView: View {
         Section {
             ZStack {
                 ScrollView(.horizontal) {
-                    HStack(spacing: 8) {
+                    HStack(spacing: 12) {
                         ForEach(categories, id: \.self) { category in
-                            CategoryCellView(
-                                title: category.plural.capitalized,
-                                imageName: Constants.randomImage)
-                            .anyButton {
-                                
+                            let imageName = popularAvatars.first(where: { $0.characterOption == category })?.profileImageName
+                            if let imageName {
+                                CategoryCellView(
+                                    title: category.plural.capitalized,
+                                    imageName: imageName)
+                                .anyButton {
+                                    onCategoryPressed(category: category, imageName: imageName)
+                                }
                             }
                         }
                     }
@@ -81,13 +99,23 @@ struct ExploreView: View {
                     title: avatar.name,
                     subtitle: avatar.characterDescription)
                 .anyButton(.highlight) {
-                    
+                    onAvatarPressed(avatar: avatar)
                 }
                 .removeListRowFormatting()
             }
         } header: {
             Text("Popular")
         }
+    }
+    
+    // MARK: - onAvatarPressed
+    private func onAvatarPressed(avatar: AvatarModel) {
+        path.append(.chat(avatarId: avatar.avatarId))
+    }
+    
+    // MARK: - onCategoryPressed
+    private func onCategoryPressed(category: CharacterOption, imageName: String) {
+        path.append(.category(category: category, imageName: imageName))
     }
 }
 
