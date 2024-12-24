cee97688e7e5bbae2d47cb3f1564c7b57072b848
Elaidzha1940 <eli080293@gmail.com>
2024-12-24 05:42:23 +0300

    feat: Add Firebase

--- EchoAI/Core/EchoAIApp.swift
+++ EchoAI/Core/EchoAIApp.swift
@@ -8,12 +8,23 @@
 //  */
 
 import SwiftUI
+import Firebase
 
 @main
 struct EchoAIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             EchoView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
     }
 }
